09922e32ca5ce45efcb97197640392b8db3b0e85
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-11-02 03:00:30 +0100

    add styleguide

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,12 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+// styleguide
+mix.js('resources/js/styleguide.js', 'public/js')
+    .sass('resources/sass/styleguide.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version()
+        .polyfill() // Support IE 11
+}
