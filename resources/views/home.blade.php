8e469fb7caadbb2c651a60081402fe19a5d82ec3
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-11-02 02:11:11 +0100

    add fonts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,11 @@
     <div class="row justify-content-center h-100">
         <div class="col-md-4 bg-primary"></div>
         <div class="col-md-4">
-            Pro knihy <br>
-            pro sbírku<br>
-            pro radost
+            <h1>
+                Pro knihy <br>
+                pro sbírku<br>
+                pro radost
+            </h1>
         </div>
         <div class="col-md-4 bg-secondary"></div>
     </div>
