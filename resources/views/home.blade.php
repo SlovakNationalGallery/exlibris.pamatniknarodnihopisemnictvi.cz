78a867791db3d4dc6b6efcb8b7f281688f7ccf4f
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-11-02 02:22:26 +0100

    set proper colors + layout according the design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid h-100">
+<div class="container-fluid h-100 bg-info ">
     <div class="row justify-content-center h-100">
-        <div class="col-md-4 bg-primary"></div>
-        <div class="col-md-4">
+        <div class="col-md-4 bg-warning"></div>
+        <div class="col-md-4 align-self-center">
             <h1>
                 Pro knihy <br>
                 pro sbírku<br>
                 pro radost
             </h1>
+            <p class="mt-5">&nbsp;</p>
+            <p class="mt-5">
+                XVI. Trienále českého ex libris 2020<br>
+                Přehlídka současné tvorby ex libris 2018–2020<br>
+                Památník národního písemnictví
+            </p>
         </div>
-        <div class="col-md-4 bg-secondary"></div>
+        <div class="col-md-4 bg-danger"></div>
     </div>
 </div>
 @endsection
