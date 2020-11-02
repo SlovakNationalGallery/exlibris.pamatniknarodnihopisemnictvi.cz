3697f2d425ec73e6119f3108f55aafc62adc2378
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-11-02 01:23:09 +0100

    basic landing page

---
+++ resources/lang/cs/app.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'title' => 'XVI. Trienále českého ex libris 2020',
+    'description' => 'Virtuální prezentace moderního exlibris spojované s knižní kulturou i žánrem volné grafiky.',
+];
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid h-100">
+    <div class="row justify-content-center h-100">
+        <div class="col-md-4 bg-primary"></div>
+        <div class="col-md-4">
+            Pro knihy <br>
+            pro sbírku<br>
+            pro radost
+        </div>
+        <div class="col-md-4 bg-secondary"></div>
+    </div>
+</div>
+@endsection
