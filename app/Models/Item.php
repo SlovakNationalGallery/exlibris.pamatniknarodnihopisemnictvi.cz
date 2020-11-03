edf14d3881b0697f97286cb379a7e209d3b6fee8
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-11-03 01:24:35 +0100

    make items searchable

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use ScoutElastic\Searchable;
 
 class Item extends Model
 {
     use HasFactory;
+    use Searchable;
+
     protected $connection = 'webumenia';
+    protected $indexConfigurator = \App\Elasticsearch\ItemsIndexConfigurator::class;
 }
