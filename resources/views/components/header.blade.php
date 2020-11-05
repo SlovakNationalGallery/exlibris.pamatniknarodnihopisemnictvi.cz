c3f1f50109726248586fb4b10d18b3031966a825
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-11-05 14:50:07 +0100

    merge master

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,25 @@
+<header class="container-fluid mb-6 navbar-expand-lg navbar-light">
+    <div class="row no-gutters">
+        <div class="col-sm-8 col-lg-3 order-lg-2">
+            <p class="lead mt-3">
+                Pro knihy, pro sbírku, pro radost<br>
+                XVI. Trienále českého ex libris 2020<br>
+                Přehlídka současné tvorby 2018–2020
+            </p>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+
+        <div class="col-lg-9 order-lg-1">
+            <nav class="collapse navbar-collapse">
+                <ul>
+                    <li class="{{ Route::is('index') ? 'active' : '' }}"><a href="{{ route('index') }}">Pro knihy</a></li>
+                    <li class=""><a href="#">pro sbírku</a></li>
+                    <li class=""><a href="#">pro radost</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,161 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="container-fluid">
+        <div class="row no-gutters">
+            <div class="col-lg-6 offset-lg-3">
+                <hr>
+
+                <h2 class="mt-1">ex libris</h2>
+
+                <article class="font-serif">
+                    <p>
+                        neboli knižní značka se řadí k tradičnímu uměleckému oboru, který prošel zajímavým his-torickým vývojem,
+                        než se z něj stala samostatná sběratelská oblast, jak jej vnímáme v dnešní době. Současné ex libris
+                        spojované s knižní kulturou i žánrem volné grafiky se přetvořilo ve specifickou výtvarnou disciplínu,
+                        které se věnují význační umělci.
+                    </p>
+                    <p>
+                        Celostátní přehlídka Trienále českého ex libris nabízí již po šestnácté možnost zhod-notit vývoj knižní
+                        značky, proměny uměleckých tendencí, ikonografické trendy a současné postupy v oblasti grafických technik
+                        za poslední tříleté období, tentokrát však netradičně i ve virtuálním prostředí.
+                    </p>
+                    <p>
+                        Výstavní soubor zahrnuje celkem 126 drobných grafik od 46 autorů včetně studentů výtvarných škol.
+                        Šestičlenná porota složená z předních odborníků udělila v letošním roce ceny deseti výtvarníkům. Mezi hlavní
+                        hodnotící kritéria patřil především důraz na umělec-kou kvalitu, vysokou úroveň výtvarného zpracování,
+                        ale také šíři a rozmanitost použitých tra-dičních i moderních grafických technik.
+                    </p>
+                    <p>
+                        Anglicky Ex libris neboli knižní značka se řadí k tradičnímu uměleckému oboru, který prošel zajímavým
+                        historickým vývojem, než se z něj stala samostatná sběratelská oblast, jak jej vnímáme v dnešní době.
+                        Současné ex libris spojované s knižní kulturou i žánrem volné grafiky se přetvořilo ve specifickou výtvarnou
+                        disciplínu, které se věnují význační umělci.
+                    </p>
+                </article>
+            </div>
+        </div>
+
+        <div class="preview mt-5 mb-6">
+            <a class="m-1" href="#">
+                <img src="http://test.webumenia.sk/dielo/nahlad/CZE:PNP.1_2020_F-110/400" alt="">
+            </a>
+            <a class="m-1" href="#">
+                <img src="http://test.webumenia.sk/dielo/nahlad/CZE:PNP.1_2020_F-126/400" alt="">
+            </a>
+            <a class="m-1" href="#">
+                <img src="http://test.webumenia.sk/dielo/nahlad/CZE:PNP.1_2020_F-126/400" alt="">
+            </a>
+        </div>
+
+        <div class="row no-gutters">
+            <div class="col-lg-6 offset-lg-3">
+                <hr>
+
+                <h2 class="mt-1">ocenění</h2>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <dl class="mt-5 mb-6">
+                            <dt class="mt-5">Grand Prix</dt>
+                            <dd>Barbora Bieylonovič</dd>
+
+                            <dt class="mt-5">Cena za přínos českému ex libris</dt>
+                            <dd>Petr Palma</dd>
+
+                            <dt class="mt-5">Cena XVI. Trienále českého ex libris 2020</dt>
+                            <dd>Martin Raudenský<br>Jan Melena</dd>
+
+                            <dt class="mt-5">Cena knihovny národního múzea</dt>
+                            <dd>Jan Kavan</dd>
+
+                            <dt class="mt-5">Cena spolku sběratelů a přátel exlibris</dt>
+                            <dd>Hana Storchová</dd>
+                        </dl>
+                    </div>
+                    <div class="col-md-6">
+                        <dl class="mt-5 mb-6">
+                            <dt class="mt-5">Cena SČUG Hollar</dt>
+                            <dd>Petr Alois Hampl</dd>
+
+                            <dt class="mt-5">Cena památníku národního písemnictví</dt>
+                            <dd>Martin Mulač</dd>
+
+                            <dt class="mt-5">Ve studentské kategorii</dt>
+
+                            <dt class="mt-4">Cena XVI. Trienále českého ex libris 2020</dt>
+                            <dd>Petr Klíma (Vyšší odborná škola grafická v Praze)</dd>
+
+                            <dt class="mt-5">Cena spolku sběratelů a přátel exlibris</dt>
+                            <dd>Martina Shořovská (Univerzita J. E. Purkyně v Ústí nad Labem)</dd>
+                        </dl>
+                    </div>
+                </div>
+
+                <hr>
+
+                <article class="mt-6 mb-5 font-serif">
+                    <p>...</p>
+                    <p class="mt-4 text-right">Zdeněk Freisleben, ředitel PNP</p>
+                </article>
+
+                <hr>
+
+                <h3 class="mt-2">Odborná porota<br>XVI. Trienále českého ex libris 2020</h3>
+
+                <dl class="mt-5 mb-6">
+                    <dt class="pt-1 mt-3 border-top">Předseda</dt>
+                    <dd>Pavel Piekar</dd>
+
+                    <dt class="pt-1 mt-3 border-top">Členové</dt>
+                    <dd>Felix Černoch, Vilma Hubáčková, Martin Mulač, Jana Solomonová, Martina Vyšohlídová</dd>
+
+                    <dt class="pt-1 mt-3 border-top">Tajemnice</dt>
+                    <dd>Barbora Vlášková</dd>
+
+                    <dt class="pt-1 mt-3 border-top">Asistentka tajemnice</dt>
+                    <dd>Jana Färberová</dd>
+                </dl>
+
+                <hr>
+
+                <article class="mt-6 font-serif">
+                    <p>
+                        Ex libris neboli knižní značka se řadí k tradičnímu umělec-kému oboru, který prošel zajímavým historickým vývojem, než se z něj stala
+                        samostatná sběratelská oblast, jak jej vnímáme v dnešní době. Současné ex libris spojované s knižní kulturou
+                        i žánrem volné grafiky se přetvořilo ve specifickou výtvarnou disciplínu, které se věnují význační umělci.
+                    </p>
+                    <p>
+                        Celostátní přehlídka Trienále českého ex libris nabízí již po šestnácté možnost zhodnotit vývoj knižní
+                        značky, proměny uměleckých tendencí, ikonografické trendy a současné postupy v oblasti grafických technik
+                        za poslední tříleté období, tentokrát však netradičně i ve virtuálním prostředí.
+                    </p>
+                    <p>
+                        Výstavní soubor zahrnuje celkem 126 drobných grafik od 46 autorů včetně studen-tů výtvarných škol.
+                        Šestičlenná porota složená z předních odborníků udělila v letošním roce ceny deseti výtvarníkům. Mezi hlavní
+                        hodnotící kritéria patřil především důraz na uměleckou kvalitu, vysokou úroveň výtvarného zpracování,
+                        ale také šíři a rozmanitost použitých tradičních i moderních grafických technik.
+                    </p>
+                    <p>
+                        Anglicky Ex libris neboli knižní značka se řadí k tradičnímu uměleckému oboru, který prošel zajímavým
+                        historickým vývojem, než se z něj stala samostatná sběratelská oblast, jak jej vnímáme v dnešní době.
+                        Současné ex libris spojované s knižní kulturou i žánrem volné grafiky se přetvořilo ve specifickou výtvarnou
+                        disciplínu, které se věnují význační umělci.
+                    </p>
+                    <p>
+                        Celostátní přehlídka Trienále českého ex libris nabízí již po šestnácté možnost zhodnotit vývoj knižní
+                        značky, proměny uměleckých tendencí, ikonografické trendy a současné postupy v oblasti grafických technik
+                        za poslední tříleté období, tentokrát však netradičně i ve virtuálním prostředí.
+                    </p>
+                    <p>
+                        Výstavní soubor zahrnuje celkem 126 drobných grafik od 46 autorů včetně studentů vý-tvarných škol.
+                        Šestičlenná porota složená z předních odborníků udělila v letošním roce ceny deseti výtvarníkům. Mezi hlavní
+                        hodnotící kritéria patřil především důraz na umě-leckou kvalitu, vysokou úroveň výtvarného zpracování,
+                        ale také šíři a rozmanitost použitých tradičních i moderních grafických technik.
+                    </p>
+                    <p class="mt-4 text-right">Pavel Piekar, předseda poroty</p>
+                </article>
+            </div>
+        </div>
+    </main>
+@endsection
