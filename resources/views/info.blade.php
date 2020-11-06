19edf7384c6c5a8da7423ef84a52868d5bad11a3
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2020-11-06 22:01:57 +0100

    [info] markup and styles

---
+++ resources/views/info.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="container-fluid">
+        <div class="row no-gutters">
+            <div class="col-lg-6 offset-lg-3">
+                <hr>
+
+                <h3 class="mt-2">
+                    Poslechněte si, u jakých skladeb tvoří účastníci<br>
+                    XVI. Trienále českého ex libris 2020
+                </h3>
+
+                <div class="mt-5 mb-6">
+                    @foreach($media as $medium)
+                        <p class="lead mb-1">
+                            <span class="font-weight-bold">{{ $loop->iteration }}. {{ $medium['person'] }}</span>,
+                            {{ $medium['relation'] }}:<br>
+                            <span class="font-weight-bold">{{ $medium['title'] }}</span>
+                            {{ $medium['subtitle'] }}
+                        </p>
+
+                        <div class="container-fluid mb-5">
+                            <div class="row">
+                                <div class="col-md-6 embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item"
+                                            src="{{ $medium['source'] }}">
+                                    </iframe>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+
+                <hr>
+
+                <article class="mt-4 mb-6 lead">
+                    <p>
+                        „Knihtisk jako archaická tisková technika dnes spadá do umělecké grafiky obdobně jako linoryt,
+                        suchá jehla či lept – grafické techniky, které používají autoři ex libris.  Díky této ruční výrobě
+                        obálky katalogu, diplomů, pozvánek či desek s pamětními listy můžeme používat různé barvy grafických
+                        kartonů. Trienále ex libris obesílají převážně umělci, kteří se věnují grafické tvorbě, a studenti
+                        výtvarných škol. Ačkoliv tvorba ex libris je disciplína mizející, trienále vytváří prostor, kde
+                        se může uplatnit i grafická tvorba menších rozměrů, a tak trvale podporuje vznik grafických děl.
+                        Soutěžní výběr pak ukazuje mnohost přístupů a různé druhy výtvarného projevu v současné grafické
+                        tvorbě.“
+                    </p>
+                    <p class="mt-4 text-right">Helena Šantavá, grafička XVI. Trienále českého ex libris 2020</p>
+                </article>
+
+                <img class="img-fluid my-3" src="{{ asset('img/pro_radost_1.jpg') }}">
+                <img class="img-fluid my-3" src="{{ asset('img/pro_radost_2.jpg') }}">
+                <img class="img-fluid my-3" src="{{ asset('img/pro_radost_3.jpg') }}">
+                <img class="img-fluid my-3" src="{{ asset('img/pro_radost_4.jpg') }}">
+            </div>
+        </div>
+    </main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,155 @@ Route::get('pro-sbirku', function () {
 })->name('catalog');
 
 Route::get('pro-radost', function () {
-    return view('info');
+    $mediumFactory = function ($person, $relation, $source, $title, $subtitle = null) {
+        return compact('person', 'relation', 'title', 'subtitle', 'source');
+    };
+
+    $media = [
+        $mediumFactory(
+            'Barbora Bieylonovič',
+            'Cena Grand Prix',
+            'https://www.youtube.com/embed/qistoCYXk9k',
+            'Maya Jane Coles – Run to You',
+            '(feat. Claudia Kane)'
+        ),
+        $mediumFactory(
+            'Petr Palma',
+            'Cena za přínos českému ex libris',
+            'https://www.youtube.com/embed/BqUhZW-46bE',
+            'Tři sestry – Expres do Bohnic'
+        ),
+        $mediumFactory(
+            'Martin Raudenský',
+            'Cena XVI. Trienále českého ex libris 2020',
+            'https://www.youtube.com/embed/XYc0qjAhWoM',
+            'FM BELFAST - Underwear'
+        ),
+        $mediumFactory(
+            'Jan Melena',
+            'Cena XVI. Trienále českého ex libris 2020',
+            'https://www.youtube.com/embed/tRMZ',
+            'David Bowie – Space Oddity'
+        ),
+        $mediumFactory(
+            'Jan Kavan',
+            'Cena knihovny Národního muzea',
+            'https://www.youtube.com/embed/We3Qv98zSIc',
+            'Jiří Suchý a Semafor Girls – Jó, to jsem ještě žil'
+        ),
+        $mediumFactory(
+            'Hana Storchová',
+            'Cena spolku sběratelů a přátel ex libris',
+            'https://www.youtube.com/embed/GRxofEmo3HA',
+            'Antonio Vivaldi – Čtvero ročních období'
+        ),
+        $mediumFactory(
+            'Petr Alois Hampl',
+            'Cena SČUG Hollar',
+            'https://www.youtube.com/embed/WgKVy9BaMuM',
+            'Eva Olmerová – Čekej tiše'
+        ),
+        $mediumFactory(
+            'Martin Mulač',
+            'Cena Památníku národního písemnictví, porotce',
+            'https://www.youtube.com/embed/KRw3cGNOGac',
+            'Mortal Cabinet – Crowley'
+        ),
+        $mediumFactory(
+            'Petr Klíma',
+            'Cena XVI. trienále českého ex libris 2020 (studentská kategorie)',
+            'https://www.youtube.com/embed/KnzJtm6h9SE',
+            'John Frusciante – Murderers'
+        ),
+        $mediumFactory(
+            'Martina Shořovská',
+            'Cena spolku sběratelů a přátel ex libris (studentská kategorie)',
+            'https://www.youtube.com/embed/BA4rSO-h9Io',
+            'Redbone – Come and Get Your Love'
+        ),
+        $mediumFactory(
+            'Pavel Piekar',
+            'předseda poroty',
+            'https://www.youtube.com/embed/zq49rymjvNg',
+            'Monteverdi – Zefiro Torna'
+        ),
+        $mediumFactory(
+            'Felix Černoch',
+            'porotce',
+            'https://www.youtube.com/embed/wOJdsMcmpDw',
+            'Leoš Janáček - Lašské písně'
+        ),
+        $mediumFactory(
+            'Vilma Hubáčková',
+            'vedoucí uměleckých sbírek PNP',
+            'https://www.youtube.com/embed/H3fSpllkUtA',
+            'Psí vojáci – Žiletky'
+        ),
+        $mediumFactory(
+            'Jana Solomonová',
+            'porotkyně',
+            'https://www.youtube.com/embed/fyMhvkC3A84',
+            'Coldplay – Every teardrop is a waterfall'
+        ),
+        $mediumFactory(
+            'Martina Vyšohlídová',
+            'porotkyně',
+            'https://www.youtube.com/embed/nkcm5cT9gGI',
+            '2Cellosc – I will wait'
+        ),
+        $mediumFactory(
+            'Barbora Vlášková',
+            'tajemnice',
+            'https://www.youtube.com/embed/mE45pn',
+            'Mirror – Nostalgia',
+            '(feat. Dave Gahan)'
+        ),
+        $mediumFactory(
+            'Jana Färberová',
+            'asistentka tajemnice',
+            'https://www.youtube.com/embed/NGorjBVag0I',
+            'Leonard Cohen – Dance Me to the End of Love'
+        ),
+        $mediumFactory(
+            'Helena Šantavá',
+            'grafika',
+            'https://www.youtube.com/embed/f1CNNf9iU9Y',
+            'Arvö Part – Salve Regina'
+        ),
+        $mediumFactory(
+            'Jaromír Štoural',
+            'knihtisk',
+            'https://www.youtube.com/embed/KUmZp8pR1uc',
+            'Amy Winehouse – Rehab'
+        ),
+        $mediumFactory(
+            'Kateřina Kostková',
+            'produkce',
+            'https://www.youtube.com/embed/pPdOo2E5zOc',
+            'The Limiñanas – Calentita',
+            '(feat. Nuria)'
+        ),
+        $mediumFactory(
+            'Aneta Křižková',
+            'propagace',
+            'https://www.youtube.com/embed/5soixb2U6xM',
+            'Cigarettes After Sex – Affection'
+        ),
+        $mediumFactory(
+            'Alžběta Ambrožová',
+            'překlad do angličtiny',
+            'https://www.youtube.com/embed/ak0yZ1vgy6o',
+            'Noname – Don\'t Forget About Me',
+        ),
+        $mediumFactory(
+            'Mariana Dočekalová',
+            'doprovodný program',
+            'https://www.youtube.com/embed/O52jAYa4Pm8',
+            'Taking Head – Psycho Killer'
+        ),
+    ];
+
+    return view('info', compact('media'));
 })->name('info');
 
 Route::get('styleguide', [StyleGuideController::class, 'index'])->name('styleguide');
