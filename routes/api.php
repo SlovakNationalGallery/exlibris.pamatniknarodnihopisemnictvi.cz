97efbefb7d560e651f62c63cd7028afcc0f7522f
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2020-11-06 18:32:35 +0100

    [api] items, author routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Scout\Builder;
+use App\Models\Item;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('authors', function () {
+    $items = Item::boolSearch()
+        ->filter('term', ['related_work' => 'XVI. Trienále českého ex libris 2020'])
+        ->aggregate('author', [
+            'terms' => [
+                'field' => 'author',
+                'size' => 50, // ¯\_(ツ)_/¯
+            ]
+        ])
+        ->execute();
+
+    return response()->json($items->aggregations()['author']['buckets']);
+});
+
+Route::get('items', function (Request $request) {
+    $author = $request->get('author');
+    $page = $request->get('page');
+
+    $items = Item::search()
+        ->where('related_work', 'XVI. Trienále českého ex libris 2020')
+        ->when($author !== null, function (Builder $builder) use ($author) {
+            return $builder->where('author', $author);
+        })
+        ->paginate(1, 'page', $page);
+
+    return response()->json($items);
 });
