100e1ccf1c52d9e38888b7a7b56eec9f097998ee
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-11-10 01:55:53 +0100

    [header] navbar-toggler style

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,15 +1,14 @@
-<header class="container-fluid mb-6 navbar-expand-lg navbar-light">
+<header class="container-fluid mb-3 mb-md-6 navbar-expand-lg navbar-light">
     <div class="row no-gutters">
         <div class="col-sm-8 col-lg-3 order-lg-2">
+            <button class="navbar-toggler text-dark mt-3 p-0 border-0" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+                <i class="fa fa-bars fa-2x"></i>
+            </button>
             <p class="lead mt-3">
-                Pro knihy, pro sbírku, pro radost<br>
+                <span class="d-none d-sm-inline">Pro knihy, pro sbírku, pro radost<br></span>
                 XVI. Trienále českého ex libris 2020<br>
                 Přehlídka současné tvorby 2018–2020
             </p>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="navbar-toggler-icon"></span>
-            </button>
         </div>
 
         <div class="col-lg-9 order-lg-1">
