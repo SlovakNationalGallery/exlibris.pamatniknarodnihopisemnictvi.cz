3ba3cb976dc77e36de8a222e1149239816a30a96
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-11-10 17:03:25 +0100

    [catalogue] filter items with award only

--- resources/js/components/AuthorsComponent.vue
+++ resources/js/components/AuthorsComponent.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        <item-component v-for="item in items" :firstItem="item" :count="counts[item.document.content.author[0]]" :key="item.document.content.author[0]"></item-component>
+    <div class="authors">
+        <a href="#" class="set-filter"
+                v-on:click="only_with_awards = !only_with_awards"
+                :class="{'active': only_with_awards}">ocenění</a>
+
+        <item-component v-for="item in filteredItems()" :firstItem="item" :count="counts[item.document.content.author[0]]" :key="item.document.content.author[0]"></item-component>
     </div>
 </template>
 
@@ -10,6 +14,7 @@
             return {
                 counts: {},
                 items: [],
+                only_with_awards: false,
                 endpoint_items: '/api/items?collapse=author&size=50',
                 endpoint_authors: '/api/authors?size=50'
             };
@@ -29,6 +34,19 @@
                             self.counts[author.key] = author.doc_count;
                         });
                     }));
+            },
+
+            filteredItems() {
+                if(!this.only_with_awards) return this.items;
+
+                var activeItems = [];
+                this.items.forEach(function(item) {
+                    if (item.document.content.additionals.award) {
+                        activeItems.push(item);
+                    }
+                });
+
+                return activeItems;
             }
         }
     }
